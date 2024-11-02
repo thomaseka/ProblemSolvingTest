832e77ea5bed93aaa721c13b73bf882b7ab6455c
thomaseka <thomaswastika@gmail.com>
2024-11-03 00:45:16 +0700

    add-detailed return in an array or terminal

--- AnotherTest/FizzBuzz.js
+++ AnotherTest/FizzBuzz.js
@@ -2,17 +2,29 @@ const fizzBuzz = (num) => {
   let result = [];
   for (let i = 1; i <= num; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
+      // array
       result.push("Fizz Buzz");
+      // console
+      console.log("Fizz Buzz");
     } else if (i % 3 == 0) {
+      // array
       result.push("Fizz");
+      // console
+      console.log("Fizz");
     } else if (i % 5 == 0) {
+      // array
       result.push("Buzz");
+      // console
+      console.log("Buzz");
     } else {
+      // array
       result.push(i);
+      // console
+      console.log(i);
     }
   }
   return result;
 };
 
-const result = fizzBuzz(15);
+const result = fizzBuzz(30);
 console.log(result);
